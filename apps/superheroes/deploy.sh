9b26eb45a78ef33ce253bb75f0cc8bff764cbc12
Josh Gavant <joshgavant@gmail.com>
2022-07-22 14:34:45 -0500

    add rest-heroes services

--- apps/superheroes/deploy.sh
+++ apps/superheroes/deploy.sh
@@ -3,28 +3,38 @@
 this_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 root_dir=$(cd ${this_dir}/../.. && pwd)
 if [[ -f ${root_dir}/.env ]]; then source ${root_dir}/.env; fi
+repo_dir=${this_dir}/superheroes
 
 container_image_group_name=${1:-"joshgav"}
 
-repo_dir=${this_dir}/superheroes
+namespace=superheroes
+kubectl config set-context --current --namespace ${namespace}
 
-## clone kubespray for ease of use of contrib files
 if [[ ! -e "${repo_dir}/pom.xml" ]]; then
     git clone https://github.com/quarkusio/quarkus-super-heroes.git ${repo_dir}
 fi
 
-pushd "${repo_dir}/rest-villains"
-trap "popd" EXIT
+for service in "villains" "heroes"; do
+    echo "INFO: deploying service ${service}"
+    kustomize build ${this_dir}/rest-${service} | kubectl apply -f -
 
-cp ${this_dir}/rest-villains-db.yml ${repo_dir}/rest-villains/src/main/kubernetes/knative.yml
-quarkus extension add quarkus-kubernetes-service-binding
+    pushd "${repo_dir}/rest-${service}"
+    quarkus extension add quarkus-kubernetes-service-binding
+    rm -rf ${repo_dir}/rest-${service}/src/main/kubernetes
 
-./mvnw clean package \
-    -Dquarkus.profile=knative-17 \
-    -Dquarkus.container-image.group=${container_image_group_name} \
-    -Dquarkus.kubernetes.deploy=true \
-    -DskipTests \
-    -Dquarkus.knative.namespace=superheroes \
-    -Dquarkus.kubernetes-service-binding.services.villains-db.api-version=postgres-operator.crunchydata.com/v1beta1 \
-    -Dquarkus.kubernetes-service-binding.services.villains-db.kind=PostgresCluster \
-    -Dquarkus.kubernetes-service-binding.services.villains-db.name=villains-db
+    ./mvnw clean package \
+        -Dquarkus.profile=knative-17 \
+        -Dquarkus.container-image.group=${container_image_group_name} \
+        -Dquarkus.kubernetes.deploy=true \
+        -DskipTests \
+        -Dquarkus.knative.namespace=superheroes \
+        -Dquarkus.kubernetes-service-binding.services.villains-db.api-version=postgres-operator.crunchydata.com/v1beta1 \
+        -Dquarkus.kubernetes-service-binding.services.villains-db.kind=PostgresCluster \
+        -Dquarkus.kubernetes-service-binding.services.villains-db.name=${service}-db
+
+    popd
+done
+
+
+
+
--- apps/superheroes/destroy.sh
+++ apps/superheroes/destroy.sh
@@ -1,10 +1,21 @@
 #! /usr/bin/env bash
 
+this_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+root_dir=$(cd ${this_dir}/../.. && pwd)
+if [[ -f ${root_dir}/.env ]]; then source ${root_dir}/.env; fi
+repo_dir=${this_dir}/superheroes
+
 namespace=superheroes
 kubectl config set-context --current --namespace ${namespace}
 
-db_name=villains-db
-service_name=rest-villains
-kubectl delete postgrescluster "${db_name}"
-kubectl delete services.serving.knative.dev "${service_name}"
-kubectl delete servicebindings "${service_name}-${db_name}"
+for service in "villains" "heroes"; do
+    echo "INFO: deploying service ${service}"
+    kustomize build ${this_dir}/rest-${service} | kubectl delete -f -
+
+    db_name=${service}-db
+    service_name=rest-${service}
+
+    kubectl delete postgrescluster "${db_name}"
+    kubectl delete services.serving.knative.dev "${service_name}"
+    kubectl delete servicebindings "${service_name}-${db_name}"
+done
