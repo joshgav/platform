511c91d2fcd8af3ba2a8dcb42e4e26e009453991
Josh Gavant <joshgavant@gmail.com>
2022-08-22 11:19:56 -0400

    refine servicebinding operator

--- openshift-services/sbo/deploy.sh
+++ openshift-services/sbo/deploy.sh
@@ -1,13 +1,23 @@
 #! /usr/bin/env bash
 
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+root_dir=$(cd ${this_dir}/../.. && pwd)
+if [[ -f ${root_dir}/.env ]]; then source ${root_dir}/.env; fi
+source ${root_dir}/lib/yaml.sh
+source ${root_dir}/lib/olm.sh
 
 echo "INFO: install operator subscription"
-oc apply -f ${this_dir}/subscription.yaml
-
-ready=1
-while [[ ${ready} != 0 ]]; do
-    echo "INFO: awaiting readiness of operator"
-    kubectl api-resources | grep servicebinding &> /dev/null
-    ready=$?
-done
+oc apply -f - <<EOF
+    apiVersion: operators.coreos.com/v1alpha1
+    kind: Subscription
+    metadata:
+        name: rh-service-binding-operator
+        namespace: openshift-operators
+    spec:
+        name: rh-service-binding-operator
+        source: redhat-operators
+        sourceNamespace: openshift-marketplace
+        channel: stable
+        installPlanApproval: Automatic
+EOF
+await_resource_ready "servicebinding"
