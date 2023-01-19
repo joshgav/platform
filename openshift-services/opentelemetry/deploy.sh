349c7bcff693613f7e02b453e2706f8f7da263dd
Josh Gavant <joshgavant@gmail.com>
2023-01-19 17:08:28 -0600

    add dist-tracing
    Signed-off-by: Josh Gavant <joshgavant@gmail.com>

--- openshift-services/opentelemetry/deploy.sh
+++ openshift-services/opentelemetry/deploy.sh
@@ -6,6 +6,9 @@ if [[ -e "${root_dir}/.env" ]]; then source ${root_dir}/.env; fi
 source ${root_dir}/lib/kubernetes.sh
 
 create_subscription opentelemetry-product
-await_resource_ready opentelemetry.io/v1alpha1
+await_resource_ready opentelemetrycollectors
+
+create_subscription jaeger-product
+await_resource_ready jaegers
 
 apply_kustomize_dir ${this_dir}/collector
