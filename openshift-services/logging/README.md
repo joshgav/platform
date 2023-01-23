bcd120e90ecf41e17f15a878bcd1b23d6db14ac3
Josh Gavant <joshgavant@gmail.com>
2023-01-23 16:00:55 -0600

    add OpenShift Logging
    Signed-off-by: Josh Gavant <joshgavant@gmail.com>

---
+++ openshift-services/logging/README.md
@@ -0,0 +1,3 @@
+## OpenShift Logging
+
+- After install you must configure indices in Kibana as documented here: <https://docs.openshift.com/container-platform/4.12/logging/cluster-logging-deploying.html#cluster-logging-visualizer-indices_cluster-logging-deploying>
---
+++ openshift-services/logging/deploy.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+root_dir=$(cd ${this_dir}/../.. && pwd)
+if [[ -e "${root_dir}/.env" ]]; then source ${root_dir}/.env; fi
+source ${root_dir}/lib/kubernetes.sh
+
+apply_kustomize_dir ${this_dir}/operator
+await_resource_ready "clusterlogforwarders"
+
+apply_kustomize_dir ${this_dir}/operand
