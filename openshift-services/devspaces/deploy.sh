a8dad8f3a2ebc5b006f0d30fa7b4cb22737382b2
Josh Gavant <joshgavant@gmail.com>
2023-01-23 20:57:48 -0600

    update DevSpaces

--- openshift-services/devspaces/deploy.sh
+++ openshift-services/devspaces/deploy.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env bash
 
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+root_dir=$(cd ${this_dir}/../.. && pwd)
+if [[ -f ${root_dir}/.env ]]; then source ${root_dir}/.env; fi
 source ${root_dir}/lib/kubernetes.sh
 
-create_subscription codeready-workspaces2 openshift-operators
-
-# TODO: wait for readiness
+create_subscription devspaces openshift-operators
+await_resource_ready 'checluster'
 
 echo "INFO: render and apply manifests"
 kustomize build ${this_dir}/base | oc apply -f -
