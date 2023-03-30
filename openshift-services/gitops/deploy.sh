7b97441b3033f02cfb27c714274ef9a879f8a980
Josh Gavant <4421720+joshgav@users.noreply.github.com>
2023-03-30 18:02:52 -0500

    update openshift-gitops (#19)
    Signed-off-by: Josh Gavant <joshgavant@gmail.com>

--- openshift-services/gitops/deploy.sh
+++ openshift-services/gitops/deploy.sh
@@ -3,24 +3,12 @@
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 root_dir=$(cd ${this_dir}/../.. && pwd)
 if [[ -f ${root_dir}/.env ]]; then source ${root_dir}/.env; fi
+source ${root_dir}/lib/kubernetes.sh
 
 kustomize build ${this_dir}/operator | oc apply -f -
+await_resource_ready argocd
 
-ready=false
-while ! ${ready}; do
-    oc api-resources | grep argo &> /dev/null
-    if [[ $? == 0 ]]; then
-        echo "INFO: operator is ready"
-        ready=true
-    else
-        echo "INFO: awaiting readiness of operator..."
-        sleep 1
-    fi
-done
+oc adm policy add-cluster-role-to-user cluster-admin -n openshift-gitops \
+    --serviceaccount openshift-gitops-argocd-application-controller
 
 kustomize build ${this_dir}/base | oc apply -f -
-
-oc adm policy add-cluster-role-to-user \
-    cluster-admin \
-    'system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller'
-    
