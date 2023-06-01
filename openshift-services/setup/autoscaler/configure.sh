79f0a8f361f9eb9cedb06da15863d753bb537bab
Josh Gavant <joshgavant@gmail.com>
2023-05-31 22:35:30 -0500

    add machine autoscalers
    Signed-off-by: Josh Gavant <joshgavant@gmail.com>

---
+++ openshift-services/setup/autoscaler/configure.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+root_dir=$(cd ${this_dir}/../../.. && pwd)
+if [[ -e "${root_dir}/.env" ]]; then source "${root_dir}/.env"; fi
+if [[ -e "${this_dir}/.env" ]]; then source "${this_dir}/.env"; fi
+
+infra_name=$(oc get infrastructures.config.openshift.io cluster -ojson | jq -r '.status.infrastructureName')
+base_machineset_name=${infra_name}-worker-${AWS_REGION}
+
+oc apply -f ${this_dir}/cluster-autoscaler.yaml
+
+for zone_id in "a" "b" "c"; do
+    export MACHINESET_NAME=${base_machineset_name}${zone_id}
+    echo "INFO: creating MachineAutoscaler for MachineSet ${MACHINESET_NAME}"
+    cat ${this_dir}/machine-autoscaler.yaml.tpl | envsubst | oc apply -f -
+done
