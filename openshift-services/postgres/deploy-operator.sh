66ff513bb66dadc8dc2cd014b343863c8a3cef38
Josh Gavant <joshgavant@gmail.com>
2023-06-01 10:06:57 -0500

    update openshift/postgres
    Signed-off-by: Josh Gavant <joshgavant@gmail.com>

--- openshift-services/postgres/deploy-operator.sh
+++ openshift-services/postgres/deploy-operator.sh
@@ -6,10 +6,10 @@ if [[ -e "${root_dir}/.env" ]]; then source ${root_dir}/.env; fi
 if [[ -e "${this_dir}/.env" ]]; then source ${this_dir}/.env; fi
 source ${root_dir}/lib/kubernetes.sh
 
-operator_name=cloud-native-postgresql
-operator_api_resource='postgresql.k8s.enterprisedb.io'
+operator_api_resource='postgresql.cnpg.io'
 
 ## Subscription via OLM
-echo "INFO: installing subscription for ${operator_api_resource}"
+echo "INFO: installing operator for API group ${operator_api_resource}"
 kustomize build ${this_dir}/operator | oc apply -f -
 await_resource_ready "${operator_api_resource}"
+oc adm policy add-scc-to-user -n cnpg-system --serviceaccount cnpg-manager nonroot-v2
