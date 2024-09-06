6315765141b54394c6fdbb501123f662b61283eb
Josh Gavant <joshgavant@gmail.com>
2024-09-06 08:10:16 -0500

    add github access for devspaces

--- openshift-services/devspaces/README.md
+++ openshift-services/devspaces/README.md
@@ -5,3 +5,11 @@
 - `dsc` CLI:
   - <https://github.com/redhat-developer/devspaces-chectl>
   - <https://developers.redhat.com/products/openshift-dev-spaces/download>
+
+### Ideas
+- https://developers.redhat.com/articles/2024/08/12/integrate-private-ai-coding-assistant-ollama
+- https://eclipse.dev/che/docs/stable/administration-guide/configuring-oauth-2-for-github/
+
+### Bug
+  - <https://github.com/devfile/devworkspace-operator/issues/1311>
+  - <https://access.redhat.com/solutions/7084768>
--- openshift-services/devspaces/deploy.sh
+++ openshift-services/devspaces/deploy.sh
@@ -3,9 +3,12 @@
 this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 root_dir=$(cd ${this_dir}/../.. && pwd)
 if [[ -f ${root_dir}/.env ]]; then source ${root_dir}/.env; fi
+if [[ -f ${this_dir}/.env ]]; then source ${this_dir}/.env; fi
 source ${root_dir}/lib/kubernetes.sh
 
 kustomize build ${this_dir}/operator | oc apply -f -
 await_resource_ready 'checluster'
 
 kustomize build ${this_dir}/operand | oc apply -f -
+
+cat ${this_dir}/resources/github-secret.template.yaml | envsubst | oc apply -f -
