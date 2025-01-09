08e22ac048006ba6fb4193189979a5e41e06d8aa
Josh Gavant <joshgavant@gmail.com>
2025-01-09 14:54:38 -0600

    add mtv operator

---
+++ openshift-services/mtv/README.md
@@ -0,0 +1,4 @@
+## MTV
+
+- https://access.redhat.com/documentation/en-us/migration_toolkit_for_virtualization/
+- https://github.com/kubev2v/forklift-documentation/
---
+++ openshift-services/mtv/deploy.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+root_dir=$(cd ${this_dir}/../.. && pwd)
+if [[ -e "${root_dir}/.env" ]]; then source ${root_dir}/.env; fi
+if [[ -e "${this_dir}/.env" ]]; then source ${this_dir}/.env; fi
+source ${root_dir}/lib/kubernetes.sh
+
+apply_kustomize_dir ${this_dir}/operator
+await_resource_ready "forkliftcontroller"
+
+apply_kustomize_dir ${this_dir}/operand
