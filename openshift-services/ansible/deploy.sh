5ef4a18680ff669820f920db0e082095f6a7c55a
Josh Gavant <joshgavant@gmail.com>
2025-06-23 12:04:01 -0500

    add service mesh 2 setup

--- openshift-services/ansible/deploy.sh
+++ openshift-services/ansible/deploy.sh
@@ -7,6 +7,7 @@ if [[ -f ${this_dir}/.env ]]; then source ${this_dir}/.env; fi
 source ${root_dir}/lib/kubernetes.sh
 
 ansible_namespace=aap
+ansible_instance_name=aap
 
 kustomize build ${this_dir}/operator | oc apply -f -
 await_resource_ready ansibleautomationplatforms
@@ -17,3 +18,8 @@ ensure_bucket ${ansible_hub_bucket_name} ${ansible_namespace}
 export S3_REGION=${S3_REGION:-''}
 
 apply_kustomize_dir ${this_dir}/operand
+
+echo "Awaiting successful deployment. This may take a while...."
+
+oc wait --for condition=Successful=true aap \
+    ${ansible_instance_name} -n ${ansible_namespace}
---
+++ openshift-services/service-mesh2/deploy.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+this_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+root_dir=$(cd ${this_dir}/../.. && pwd)
+if [[ -e "${root_dir}/.env" ]]; then source ${root_dir}/.env; fi
+source ${root_dir}/lib/kubernetes.sh
+
+apply_kustomize_dir ${this_dir}/operator
+await_resource_ready servicemeshcontrolplane
+apply_kustomize_dir ${this_dir}/configuration
