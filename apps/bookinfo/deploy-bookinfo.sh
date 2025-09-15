912fcaf804af8a3bbf91398094c7cf5338d36174
Josh Gavant <joshgavant@gmail.com>
2025-09-15 08:35:09 -0500

    split apps/httpbin into base and mesh

--- apps/bookinfo/deploy-bookinfo.sh
+++ apps/bookinfo/deploy-bookinfo.sh
@@ -5,7 +5,4 @@ root_dir=$(cd ${this_dir}/../.. && pwd)
 if [[ -e "${root_dir}/.env" ]]; then source ${root_dir}/.env; fi
 source ${root_dir}/lib/kubernetes.sh
 
-## OpenShift Service Mesh instructions:
-## - https://docs.redhat.com/en/documentation/red_hat_openshift_service_mesh/3.0/html/installing/ossm-installing-service-mesh#ossm-about-bookinfo-application_ossm-discoveryselectors-scope-service-mesh
-
 kustomize build ${this_dir}/resources | oc apply -f -
--- apps/httpbin/deploy.sh
+++ apps/httpbin/deploy.sh
@@ -5,4 +5,5 @@ root_dir=$(cd ${this_dir}/../.. && pwd)
 if [[ -e "${root_dir}/.env" ]]; then source ${root_dir}/.env; fi
 source ${root_dir}/lib/kubernetes.sh
 
-kustomize build ${this_dir}/resources | oc apply -f -
+kustomize build ${this_dir}/base | oc apply -f -
+# kustomize build ${this_dir}/mesh | oc apply -f -
